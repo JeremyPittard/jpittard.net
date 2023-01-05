feb969a7575dbbdc32f06d5f14106a97c6205dd5
Jeremy Pittard <jeremy@jpittard.net>
2023-01-05 08:51:24 +0800

    unsure about using a footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
     <div className="max-w-[80%] md:max-w-4xl mx-auto py-3 px-2">
-      A footer is going here
+      <nav>
+        <ul className="flex gap-4 text-sm mt-24 pt-32">
+          <li>
+            <Link href={"https://www.facebook.com/Jpittardweb"} target="_blank">
+              fb
+            </Link>
+          </li>
+          <li>
+            <Link href={"/home"}>home</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
