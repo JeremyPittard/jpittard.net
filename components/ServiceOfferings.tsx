7a4bb67fc6dc5f5b1a26dde6918261bc37e54f7d
Jeremy Pittard <jeremy@jpittard.net>
2023-01-19 14:26:49 +0800

    border opacity on footer, vertical padding on ServiceOfferings component

--- components/ServiceOfferings.tsx
+++ components/ServiceOfferings.tsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 const ServiceOfferings = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between pt-32">
+    <div className="flex flex-col md:flex-row justify-between py-32">
       <Card title={`DIY Solutions`} link="/diy-solutions">
         <p>
           Tools to help identify and fix issues with accessibility, page speed
