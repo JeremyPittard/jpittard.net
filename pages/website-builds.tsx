237e1cd1719fdd01cee61e10b39107cb4a19dc1b
Jeremy Pittard <jeremy@jpittard.net>
2023-01-24 10:41:56 +0800

    sizing

--- pages/website-builds.tsx
+++ pages/website-builds.tsx
@@ -4,7 +4,7 @@ import ServiceOfferings from "../components/ServiceOfferings";
 const websiteBuild = () => {
   return (
     <Layout>
-      <div className="max-w-[80%] md:max-w-4xl mx-auto mb-24">
+      <div className="max-w-[80%] md:max-w-3xl mx-auto mb-24">
         <h1 className="pt-12 pb-6">Complete Builds</h1>
         <p></p>
       </div>
