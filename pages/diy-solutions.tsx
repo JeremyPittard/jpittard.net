03266ee81a12c7aafb16f9929d0012ccde56a4c4
Jeremy Pittard <jeremy@jpittard.net>
2023-01-24 10:46:33 +0800

    spacing

--- pages/diy-solutions.tsx
+++ pages/diy-solutions.tsx
@@ -5,7 +5,7 @@ import ServiceOfferings from "../components/ServiceOfferings";
 const diySolutions = () => {
   return (
     <Layout>
-      <div className="max-w-[80%] md:max-w-3xl mx-auto mb-24 p-6">
+      <div className="max-w-[80%] md:max-w-3xl mx-auto mb-24 pb-6">
         <p className="py-6">
           Printable workbooks, guides and templates to help improve your sites
           accessibility, core web vitals and SEO with no code.
