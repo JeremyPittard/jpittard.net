9c4e65dd0f8130f6c96c01061a61fdff5c2bde3c
Jeremy Pittard <jeremy@jpittard.net>
2023-03-14 06:59:01 +0800

    removed unnceccesary line

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import Head from "next/head";
-import { Content, Inter } from "@next/font/google";
 import Layout from "../components/Layout";
 import { motion } from "framer-motion";
 import IntroContent from "../components/IntroContent";
 import ServiceOfferings from "../components/ServiceOfferings";
 import RecommendedServicesIntro from "../components/RecommendedServicesIntro";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -15,7 +12,7 @@ export default function Home() {
         <motion.main>
           <IntroContent />
           <ServiceOfferings />
-          {/* <RecommendedServicesIntro /> */}
+          <RecommendedServicesIntro />
         </motion.main>
       </Layout>
     </>
