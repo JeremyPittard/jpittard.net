c46e40a24503df210b96f4c16aedab53a2c0276a
Jeremy Pittard <jeremy@jpittard.net>
2023-03-16 14:22:38 +0800

    tidy up 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,13 @@
+import Layout from "../components/Layout";
+import ServiceOfferings from "../components/ServiceOfferings";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <Layout>
+      <div className="max-w-[80%] md:max-w-3xl mx-auto mb-24 pb-6">
+        <h1>404 - Page Not Found</h1>
+      </div>
+      <ServiceOfferings />
+    </Layout>
+  );
 }
